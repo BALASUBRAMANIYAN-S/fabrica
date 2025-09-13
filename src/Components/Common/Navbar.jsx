8fa5d131b5450e8455e0945962b9301aac9fced1
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-13 19:50:37 +0530

    Refactor Navbar component: improve structure and styling, add state management

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,11 +1,18 @@
+"use client";
+import { useState } from "react";
+
 function Navbar() {
   const NavNAme = ["Studio", "Projects", "Blog", "Contact"];
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="bg-gray-200">
       <div className="flex flex-row font-bold py-4 items-center justify-between px-10 list-none">
-        <li className="text-[18px] flex items-center gap-1">
+        <li className="text-[18px] flex items-center ">
           fabrica
-          <span className="text-[8px] border border-black rounded-4xl px-[3px]">R</span>
+          <span className="text-[6px] mb-2 border border-black rounded-4xl px-[3px]">
+            R
+          </span>
         </li>
 
         {NavNAme.map((Values, index) => {
@@ -15,8 +22,8 @@ function Navbar() {
             </li>
           );
         })}
-        <li className="flex justify-center items-center flex-col gap-2 cursor-pointer">
-          <div className="border-b-3 px-7 "></div>
+          <li className="flex justify-center items-center flex-col gap-2 cursor-pointer">
+          <div className="border-b-3  px-7 "></div>
           <div className="border-b-3 px-7"></div>
         </li>
       </div>
