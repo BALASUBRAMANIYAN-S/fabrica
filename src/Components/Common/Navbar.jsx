e306264ed5ed781168127dd2b6193cbb0e6a126a
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-13 19:39:34 +0530

    Refactor Navbar component structure and update branding

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,14 +1,25 @@
 function Navbar() {
-    const NavNAme=["Studio","Projects","Blog","Contact"];
+  const NavNAme = ["Studio", "Projects", "Blog", "Contact"];
   return (
     <div className="bg-gray-200">
-    <div className="flex flex-row font-bold py-4 items-center justify-between px-10 list-none">
-      <li className="text-[18px]" >Dezvolta</li>
-      {NavNAme.map((Values,index)=>{
-        return <li className="cursor-pointer" key={index}>{Values}</li>
-      })}
-      <li className="flex justify-center items-center flex-col gap-2 cursor-pointer"><div className="border-b-3 px-7 "></div><div className="border-b-3 px-7"></div></li>
-    </div>
+      <div className="flex flex-row font-bold py-4 items-center justify-between px-10 list-none">
+        <li className="text-[18px] flex items-center gap-1">
+          fabrica
+          <span className="text-[8px] border border-black rounded-4xl px-[3px]">R</span>
+        </li>
+
+        {NavNAme.map((Values, index) => {
+          return (
+            <li className="cursor-pointer" key={index}>
+              {Values}
+            </li>
+          );
+        })}
+        <li className="flex justify-center items-center flex-col gap-2 cursor-pointer">
+          <div className="border-b-3 px-7 "></div>
+          <div className="border-b-3 px-7"></div>
+        </li>
+      </div>
     </div>
   );
 }
