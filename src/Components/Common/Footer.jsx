525ac8e2a454659918f297ce0db7cf9f4ca748aa
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-14 22:41:21 +0530

    Add a footer

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,7 +1,11 @@
 
 function Footer() {
   return (
-    <div></div>
+    <div>
+      <footer className="bg-gray-500 text-white text-center py-10">
+      <p>© 2025 fabrica® Studio. All rights reserved.</p>
+      </footer>
+    </div>
   )
 }
 
