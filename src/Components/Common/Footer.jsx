cb659f884a09cd7bccd3835215a009317c46c426
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-14 22:48:04 +0530

    Refactor Footer component: update background color and structure

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,12 +1,17 @@
-
 function Footer() {
   return (
     <div>
-      <footer className="bg-gray-500 text-white text-center py-10">
-      <p>© 2025 fabrica® Studio. All rights reserved.</p>
+      <footer className="bg-black text-white text-center py-10">
+        <div className="flex flex-row items-center justify-between px-20">
+          <p>© 2025 fabrica® Studio. All rights reserved.</p>
+          <ul className="flex flex-row gap-5">
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+          </ul>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
