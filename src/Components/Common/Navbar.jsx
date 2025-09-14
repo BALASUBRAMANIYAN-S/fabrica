779180d32b7b56568bad8367ba98bf4dd38f8736
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-14 23:13:46 +0530

    Navbar : enhance structure and improve click handling

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -10,36 +10,36 @@ function Navbar() {
     <div>
       <div className="py-4">
         <ul className="flex justify-between px-5">
-     
-          <li className="text-[18px] flex items-center">
+          <li className="text-[18px] z-20 flex items-center">
             fabrica
             <span className="text-[6px] border border-black rounded-4xl px-[3px]">
               R
             </span>
           </li>
 
-          
           {!isClicked && (
             <>
               {NavNAme.map((value, index) => (
-                <li key={index}>{value}</li>
+                <div className="flex gap-5">
+                  <li key={index}>{value}</li>
+                </div>
               ))}
             </>
           )}
 
-          
           {isClicked && (
-            <ul className="flex flex-col">
+            <div className="absolute z-10 top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center gap-6 text-2xl font-semibold ">
               {NavNAmeTwo.map((value, index) => (
-                <li key={index}>{value}</li>
+                <li key={index} className="list-none cursor-pointer hover:text-gray-500">
+                  {value}
+                </li>
               ))}
-            </ul>
+          </div>
           )}
 
-         
           <li
             onClick={() => setIsClicked(!isClicked)}
-            className="flex justify-center items-center flex-col gap-2 cursor-pointer"
+            className="flex z-20 justify-center items-center flex-col gap-2 cursor-pointer"
           >
             <div className="border-b-3 px-7"></div>
             <div className="border-b-3 px-7"></div>
